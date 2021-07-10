c5177c99f8391c914851aea65b16117bfb68f31e
savanna chiang <savannaac@gmail.com>
2021-07-09 20:25:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+// equals 62
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+// generate random integer greater than 0
+let random = Math.floor(Math.random() * 10)
+// let x = 0
+// let random = x += 1
+
+// remainder equal to 4
+let num3 = 8
+let num4 = 2
+let mod = num3 / num4
+
+//return 20 as highest number
+let max = Math.max(5, 10, 15, 20)
+
+
